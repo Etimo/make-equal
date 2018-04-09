fcd6d4b54497db658db921347f8e2c3bb7576fa0
VincentXis <rickard.bernal@gmail.com>
2018-04-09 16:32:03 +0200

    made the frontpage a class because i need to use state

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,36 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {SectionedTextContainer, TextColumn, TextRow} from "./layout/Layout-components";
 import {frontPageSectionOne, frontPageSectionTwo, frontPageTitle} from '../resources/other/page-text-content';
 import {Button} from "semantic-ui-react";
 import {GetWordsFromExternalApi} from "./external-api/MakeEqualAnalyticsWordsApi";
 import WordDefinitionModal from './WordDefinistionModal';
 
-const FrontPage = (props) => {
-
-  return (
-    <SectionedTextContainer>
-      <TextRow>
-        <TextColumn width={10}>
-          {frontPageTitle}
-          {frontPageSectionOne}
-        </TextColumn>
-      </TextRow>
-      <TextRow>
-        <TextColumn width={10}>
-          {frontPageSectionTwo}
-        </TextColumn>
-      </TextRow>
-      <TextRow>
-        <TextColumn centered>
-          <Button content={"Tryck här för att komma igång"} onClick={() => props.showForm()}/>
-        </TextColumn>
-      </TextRow>
-      <TextRow>
-        <TextColumn centered>
-          <WordDefinitionModal word={"wawa"} wordDefinition={"something i say"}/>
-        </TextColumn>
-      </TextRow>
-    </SectionedTextContainer>
-  );
-};
+class FrontPage extends Component{
+  constructor () {
+    super();
+  }
+  render () {
+    return (
+      <SectionedTextContainer>
+        <TextRow>
+          <TextColumn width={10}>
+            {frontPageTitle}
+            {frontPageSectionOne}
+          </TextColumn>
+        </TextRow>
+        <TextRow>
+          <TextColumn width={10}>
+            {frontPageSectionTwo}
+          </TextColumn>
+        </TextRow>
+        <TextRow>
+          <TextColumn centered>
+            <Button content={"Tryck här för att komma igång"} onClick={() => this.props.showForm()}/>
+          </TextColumn>
+        </TextRow>
+        <TextRow>
+          <TextColumn centered>
+            <WordDefinitionModal word={"wawa"} wordDefinition={"something i say"}/>
+          </TextColumn>
+        </TextRow>
+      </SectionedTextContainer>
+    );
+  }
+}
 export default FrontPage;
