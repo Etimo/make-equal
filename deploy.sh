1c7d6fb9e9f89e6759e4838d1c0eae223b2df0fb
Teo Klestrup Röijezon <teo.roijezon@etimo.se>
2018-07-03 14:16:27 +0200

    Prefer IPv4

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 yarn install
 yarn build
 
-lftp -c "open -u '${LFTP_USERNAME},${LFTP_PASSWORD}' ${LFTP_SERVER} && mirror --reverse --delete build public_html"
+lftp -c "set dns:order 'inet inet6' && open -u '${LFTP_USERNAME},${LFTP_PASSWORD}' ${LFTP_SERVER} && mirror --reverse --delete build public_html"
