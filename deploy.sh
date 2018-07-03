4aefd3241c02f72358f713ee9e9d1a78f4c9d6ee
Teo Klestrup Röijezon <teo.roijezon@etimo.se>
2018-07-03 13:45:32 +0200

    Install lftp before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env sh
+yarn install
 yarn build
+
+sudo apt update
+sudo apt install lftp
 lftp -u "${LFTP_USERNAME}" --env-password "${LFTP_SERVER}" -c "mirror --reverse --delete build public_html"
