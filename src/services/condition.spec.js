3276aceebb1c7ef6b501ba28ce709e1fb82096a8
Teo Klestrup Röijezon <teo@nullable.se>
2018-07-09 17:01:20 +0200

    More complex conditional tests

--- src/services/condition.spec.js
+++ src/services/condition.spec.js
@@ -18,3 +18,93 @@ it('should support multiple-choice conditions', () => {
   expect(checkCondition('a-b', { 'a-b': true })).toBe(true);
   expect(checkCondition('a-b', { 'a-b': false })).toBe(false);
 });
+
+it('should support aggregate conditions', () => {
+  expect(
+    checkCondition(
+      {
+        all: ['a-b', 'a-c']
+      },
+      { 'a-b': true, 'a-c': true }
+    )
+  ).toBe(true);
+  expect(
+    checkCondition(
+      {
+        all: ['a-b', 'a-c']
+      },
+      { 'a-c': true }
+    )
+  ).toBe(false);
+  expect(
+    checkCondition(
+      {
+        all: ['a-b', 'a-c']
+      },
+      { 'a-c': true }
+    )
+  ).toBe(false);
+  expect(
+    checkCondition(
+      {
+        all: ['a-b', 'a-c']
+      },
+      {}
+    )
+  ).toBe(false);
+
+  expect(
+    checkCondition(
+      {
+        any: ['a-b', 'a-c']
+      },
+      { 'a-b': true, 'a-c': true }
+    )
+  ).toBe(true);
+  expect(
+    checkCondition(
+      {
+        any: ['a-b', 'a-c']
+      },
+      { 'a-c': true }
+    )
+  ).toBe(true);
+  expect(
+    checkCondition(
+      {
+        any: ['a-b', 'a-c']
+      },
+      { 'a-c': true }
+    )
+  ).toBe(true);
+  expect(
+    checkCondition(
+      {
+        any: ['a-b', 'a-c']
+      },
+      {}
+    )
+  ).toBe(false);
+});
+
+it('should support arbitrary nesting of conditions', () => {
+  expect(
+    checkCondition(
+      {
+        any: [
+          {
+            not: {
+              all: ['a-b', 'a-c']
+            }
+          },
+          {
+            not: {
+              any: ['d-a']
+            }
+          }
+        ]
+      },
+      { 'd-a': true }
+    )
+  ).toBe(true);
+});
